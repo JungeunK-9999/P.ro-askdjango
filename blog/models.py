e78ab93e868c8daca50a5d7c65aa1741f771a8b2
Jung <wjddms04@hanyang.ac.kr>
2019-07-19 17:15:52 +0900

    blog.models.Post 모델에 status필드를 추가하고Custom Admin Action 구현

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,24 @@
-#blog-models
+# blog-models
 
 import re
 from django.db import models
 from django.forms import ValidationError
+
+
 # from django.utils import timezone
 
 def lnglat_validator(value):
-    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$',value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
         raise ValidationError('Invalid LngLat Type')
+
 
 # Create your models here.
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력하세요.')
@@ -20,14 +28,13 @@ class Post(models.Model):
     #                              ('제목2', '제목2 레이블'),
     #                              ('제목3', '제목3 레이블')
     #                          ))  #길이 제한 있음
-    content = models.TextField()              #길이 제한 없음
+    content = models.TextField()  # 길이 제한 없음
 
-    tags=models.CharField(max_length=100, blank=True)
-    lnglat=models.CharField(max_length=50, blank=True ,
-                            validators=[lnglat_validator],
-                            help_text='위도/경도 포맷으로 입력하세요',
-                            )
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+                              validators=[lnglat_validator],
+                              help_text='위도/경도 포맷으로 입력하세요',
+                              )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
