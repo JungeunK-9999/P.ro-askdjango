a8eaeadd9dfcf8094090f83b264aa8014fccb093
Jung <wjddms04@hanyang.ac.kr>
2019-07-19 13:24:18 +0900

    5강 끝!

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 #blog-models
 
+import re
 from django.db import models
+from django.forms import ValidationError
+# from django.utils import timezone
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$',value):
+        raise ValidationError('Invalid LngLat Type')
 
 # Create your models here.
 class Post(models.Model):
@@ -13,6 +20,12 @@ class Post(models.Model):
     #                              ('제목3', '제목3 레이블')
     #                          ))  #길이 제한 있음
     content = models.TextField()              #길이 제한 없음
+
+    tags=models.CharField(max_length=100, blank=True)
+    lnglat=models.CharField(max_length=50, blank=True ,
+                            validators=[lnglat_validator],
+                            help_text='위도/경도 포맷으로 입력하세요',
+                            )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
