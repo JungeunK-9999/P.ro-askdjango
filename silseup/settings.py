fe49d3206d8e8f34c18662d5a495eb79867e9b15
Jung <wjddms04@hanyang.ac.kr>
2019-07-23 04:18:57 +0900

    12강 끝

--- silseup/settings.py
+++ silseup/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'silseup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'silseup', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
